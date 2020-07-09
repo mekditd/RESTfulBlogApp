b75dce324035fb20516ad818cdcdbf58834f4993
Tedla B <mekdi.td87@outlook.com>
2020-07-09 04:59:26 -0500

    setup the blogSchema

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var express    = require("express");
-mongoose       = require("mongoose");
-bodyParser     = require("body-parser");
-app            = express();
+var express        = require("express");
+    mongoose       = require("mongoose");
+    bodyParser     = require("body-parser");
+    app            = express();
 
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now};
+});
 
 
 
