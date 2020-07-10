8462fcbb323d6565b454a095b520c93c9aff71fe
Tedla B <mekdi.td87@outlook.com>
2020-07-09 23:40:35 -0500

    created public and views directories and included header, footer, and css files in the directories

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express        = require("express");
-    mongoose       = require("mongoose");
-    bodyParser     = require("body-parser");
+var express        = require("express"),
+    mongoose       = require("mongoose"),
+    bodyParser     = require("body-parser"),
     app            = express();
 
 
-mongoose.connect("mongodb://localhost/restful_blog_app");
+mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,9 +13,10 @@ var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now};
+    created: {type: Date, default: Date.now}
 });
 
+var Blog = mongoose.model("Blog", blogSchema);
 
 
 // title
@@ -23,7 +24,29 @@ var blogSchema = new mongoose.Schema({
 // body
 // created
 
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//     body: "Hello this is a Blog POST!"
+// })
+
+// RESTful routes
+
+app.get("/", function(req, res){
+    res.redirect("/blog");
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    })
+})
+
 
 app.listen(3000, function(){
-    res.send("Server is listening on PORT: 3000");
+    console.log("Server is listening on PORT: 3000");
 })
